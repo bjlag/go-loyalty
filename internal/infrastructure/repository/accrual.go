13bb9c6a9e782a46f49b39cca1b23da5b07cbb3d
Vladislav Duplin <vaduplin@avito.ru>
2024-11-08 19:29:53 +0300

    iter2: Rename `number` to `order_number`

--- internal/infrastructure/repository/accrual.go
+++ internal/infrastructure/repository/accrual.go
@@ -27,7 +27,7 @@ func NewAccrualPG(db *sqlx.DB) *AccrualPG {
 }
 
 func (r AccrualPG) AccrualByOrderNumber(ctx context.Context, orderNumber string) (*model.Accrual, error) {
-	query := "SELECT number, user_guid, status, accrual, uploaded_at FROM accruals WHERE number = $1"
+	query := "SELECT order_number, user_guid, status, accrual, uploaded_at FROM accruals WHERE order_number = $1"
 	stmt, err := r.db.PrepareContext(ctx, query)
 	if err != nil {
 		return nil, fmt.Errorf("failed to prepare query: %w", err)
@@ -38,7 +38,7 @@ func (r AccrualPG) AccrualByOrderNumber(ctx context.Context, orderNumber string)
 
 	var m accrual
 	row := stmt.QueryRowContext(ctx, orderNumber)
-	err = row.Scan(&m.Number, &m.UserGUID, &m.Status, &m.Accrual, &m.UploadedAt)
+	err = row.Scan(&m.OrderNumber, &m.UserGUID, &m.Status, &m.Accrual, &m.UploadedAt)
 	if err != nil {
 		if errors.Is(err, sql.ErrNoRows) {
 			return nil, nil
@@ -51,7 +51,7 @@ func (r AccrualPG) AccrualByOrderNumber(ctx context.Context, orderNumber string)
 }
 
 func (r AccrualPG) Insert(ctx context.Context, accrual *model.Accrual) error {
-	query := `INSERT INTO accruals (number, user_guid, status, accrual, uploaded_at) VALUES ($1, $2, $3, $4, $5)`
+	query := `INSERT INTO accruals (order_number, user_guid, status, accrual, uploaded_at) VALUES ($1, $2, $3, $4, $5)`
 	stmt, err := r.db.PrepareContext(ctx, query)
 	if err != nil {
 		return fmt.Errorf("failed to prepare query: %w", err)
@@ -60,7 +60,7 @@ func (r AccrualPG) Insert(ctx context.Context, accrual *model.Accrual) error {
 		_ = stmt.Close()
 	}()
 
-	_, err = stmt.ExecContext(ctx, accrual.Number, accrual.UserGUID, accrual.Status, accrual.Accrual, accrual.UploadedAt)
+	_, err = stmt.ExecContext(ctx, accrual.OrderNumber, accrual.UserGUID, accrual.Status, accrual.Accrual, accrual.UploadedAt)
 	if err != nil {
 		return fmt.Errorf("failed to save accrual: %w", err)
 	}
--- migrations/2_create_accruals_table.up.sql
+++ migrations/2_create_accruals_table.up.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS accruals (
-    number varchar(50) NOT NULL PRIMARY KEY,
+    order_number varchar(50) NOT NULL PRIMARY KEY,
     user_guid uuid NOT NULL REFERENCES users (guid),
     status smallint NOT NULL DEFAULT 0,
     accrual integer NOT NULL DEFAULT 0,
@@ -9,8 +9,8 @@ CREATE TABLE IF NOT EXISTS accruals (
 CREATE INDEX accruals_user_guid_fk_idx ON accruals (user_guid);
 
 COMMENT ON TABLE accruals IS 'Начисления по заказам';
-COMMENT ON COLUMN accruals.number IS 'Номер заказа';
+COMMENT ON COLUMN accruals.order_number IS 'Номер заказа';
 COMMENT ON COLUMN accruals.user_guid IS 'GUID пользователя';
 COMMENT ON COLUMN accruals.status IS 'Статус обработки начисления по заказу';
-COMMENT ON COLUMN accruals.accrual IS 'Размер начисления';
+COMMENT ON COLUMN accruals.accrual IS 'Размер начисления по заказу';
 COMMENT ON COLUMN accruals.uploaded_at IS 'Дата и время загрузки заказа';
