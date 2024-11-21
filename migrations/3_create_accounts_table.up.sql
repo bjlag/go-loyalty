6774e04b69cf7d40a4360476918bac44d82823ed
Vladislav Duplin <vaduplin@avito.ru>
2024-11-21 19:31:13 +0300

    iter2: Withdraw sum

--- migrations/3_create_accounts_table.up.sql
+++ migrations/3_create_accounts_table.up.sql
@@ -1,10 +1,12 @@
 CREATE TABLE IF NOT EXISTS accounts (
     guid uuid NOT NULL PRIMARY KEY REFERENCES users (guid),
     balance integer NOT NULL DEFAULT 0,
+    withdraw_sum integer NOT NULL DEFAULT 0,
     updated_at timestamp with time zone NOT NULL
 );
 
 COMMENT ON TABLE accounts IS 'Счета баллов лояльности пользователя';
 COMMENT ON COLUMN accounts.guid IS 'GUID счета, совпадает с GUID пользователя';
-COMMENT ON COLUMN accounts.balance IS 'Количество баллов лояльности на счету';
+COMMENT ON COLUMN accounts.balance IS 'Сумма доступных баллов лояльности на счету';
+COMMENT ON COLUMN accounts.withdraw_sum IS 'Сумма использованных за весь период регистрации баллов';
 COMMENT ON COLUMN accounts.updated_at IS 'Дата и время обновления счета';
